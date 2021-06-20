008e62ff9f3508771b252864e9149c838d8dd5f4
Jens Andersson <jens.andersson5@gmail.com>
2021-06-20 07:05:24 +0200

    Add project

---
+++ src/Tethys/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tethys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
